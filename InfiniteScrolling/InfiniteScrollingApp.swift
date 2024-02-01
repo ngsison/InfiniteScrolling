4fd65ccdeb35a3baa3066eff157c62326600a8e6
Nathaniel Brion Sison <ngsison@outlook.com>
2024-02-02 02:45:22 +0800

    Initial Commit

---
+++ InfiniteScrolling/InfiniteScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollingApp.swift
+//  InfiniteScrolling
+//
+//  Created by Nathaniel Brion Sison on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
